993f7bad87b83bffff7be163550115b3feb99f90
Sean Mangosing <seanmangosing@SeansPesonalMBP.hsd1.ca.comcast.net>
2021-01-16 19:13:48 -0800

    testing

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", function (req, res) {
+  return res.send("Hello World from node!!!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`The server has started and is listening on Port: ${PORT}.`);
+});
